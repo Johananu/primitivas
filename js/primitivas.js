9bf9d48b609aadb08b42a985f0168f530252af30
Johananu <31250023+Johananu@users.noreply.github.com>
2017-11-11 20:08:59 -0700

    Update primitivas.js
    Método para cambiar la textura al pulsar una tecla

--- js/primitivas.js
+++ js/primitivas.js
@@ -33,6 +33,29 @@
     toroide.position.x = 40;
     scene.add(toroide); 
     
+       let c=0;// c sera un contador que nos permitira saber si se volvio a pulsar una tecla
+    document.body.onkeypress= function(){//onkeypress es el metodo de presionar una tecla
+
+     if(c % 2 == 0){
+         //c se incrementara cada vez que pulses una tecla(la primera vez sera impar, la segunda par y asi sucesivamente)
+         //si c es par(% obtiene el mod) se carga la imagen 4
+         texture = new THREE.TextureLoader().load('public/p5.jpg');
+        }
+     else{//si no es par se carga la imagen 6
+         texture = new THREE.TextureLoader().load('public/p6.jpg');
+        }
+                    
+        //cambiamos el material de la primitiva por la textura que se halla cargado
+                   piramide.material= new THREE.MeshBasicMaterial({ map:texture });
+                   cubo.material= new THREE.MeshBasicMaterial({ map:texture });
+                   toroide.material= new THREE.MeshBasicMaterial({ map:texture });
+     //lo mostramos
+                   renderer.render(scene,camera);
+
+                   c=c+1;//incrementa el contador
+                   console.log(c);
+            }
+    
    //crear animacion 
     function loop(){ 
         requestAnimationFrame(loop);
